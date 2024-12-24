85bc86506431ad0d0a3e7a67aa0f4e33d57419c5
Fikret Gözütok <gozutokfikret@gmail.com>
2024-12-24 21:33:35 +0300

    Add Entities layer

---
+++ Entitites/Concrete/Author.cs
@@ -0,0 +1,10 @@
+﻿using Entitites.Abstract;
+
+namespace Entitites.Concrete
+{
+    public class Author : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+    }
+}
---
+++ Entitites/Concrete/Book.cs
@@ -0,0 +1,16 @@
+﻿using Entitites.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitites.Concrete
+{
+    public class Book : BaseEntity
+    {
+        public int AuthorID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime PublishedDate { get; set; }
+    }
+}
